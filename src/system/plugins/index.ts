58a020f7b4cfe292c5ab4bf348a9a6ee8ed1b60e
Florian Marcu <florian.mrc@gmail.com>
2023-09-25 14:53:32 -0700

    prettier

---
+++ src/system/plugins/index.ts
@@ -0,0 +1,16 @@
+/*
+
+Anatomy of a plugin:
+- metadata.json
+- api
+- app
+- assets
+- hooking system
+- data models
+- database migrations
+- routing system
+- tests
+
+*/
+
+export const installedPlugins = ['blog']
